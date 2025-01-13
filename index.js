45fc75d50a8d81389ebf47b709e6f63a53ddc202
Shiva WebDev <psivaiah2003@gmail.com>
2025-01-13 09:16:02 +0530

    Http Methods

--- index.js
+++ index.js
@@ -12,4 +12,34 @@ app.get("/", (req, res) => {
   res.send("Home Page 🤣");
 });
 
+// string Pattern:
+
+app.get("/ab?cd", (req, res) => {
+  res.send(
+    "It will be work when user hit localhost:6174/abcd or localhost:6174/acd"
+  );
+});
+
+// Advanced Routing:
+
+app.get("/api/home", (req, res) => {
+  res.send("<h2>Home Page🏠</h2>");
+});
+
+app.post("/api/users", (req, res) => {
+  res.send("<h2>User Acount Was Created.</h2>");
+});
+
+app.put("/api/users", (req, res) => {
+  res.send("<h2>User Details Was Updated Completely.</h2>");
+});
+
+app.patch("/api/users", (req, res) => {
+  res.send("<h2>User Email is Updated .</h2>");
+});
+
+app.delete("/api/users", (req, res) => {
+  res.send("<h2>User Acount Was Deleted SuccessFully.</h2>");
+});
+
 app.listen(port, () => console.log(`Server is running on port: ${port}!`));
